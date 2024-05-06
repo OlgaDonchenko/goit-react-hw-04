768ccc8b01b869b52354e2e564b98d80e1b549a2
OlgaDonchenko <n0931606045@gmail.com>
2024-05-06 21:02:45 +0300

    saved5

--- src/searcher-api.js
+++ src/searcher-api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.unsplash.com";
 
 export const fetchImages = async (query, page) => {
-  const response = await axios.get("/search/photos/", {
+  const response = await axios.get("/search/photos", {
     params: {
       client_id: "QFHtF_jB1XddBPCXNzRt9Lx9WIqL3MYsBWr77V6XOsI",
       query: query,
